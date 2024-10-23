cd5071c5215da8ece7afcb0b8c4209a8875d4dac
sailing <176530355@qq.com>
2024-10-23 21:31:28 +0800

    yu

--- MainAccess.py
+++ MainAccess.py
@@ -1,7 +1,9 @@
 from Ra import *
 from Pile import *
+from Soil import *
 import json
 import os
+import pandas as pd
 
 ra=Ra()
 
@@ -10,4 +12,14 @@ json_file_path = r'C:\Users\17653\mycodegit\mycodegit\Soildata.json'
 with open(json_file_path, 'r',encoding='utf-8') as mydata:
     mydic=json.load(mydata)
 
-print(mydic)
+#print(mydic)
+#for k, v in mydic:
+#k=Soil(v[0],v[1],v[2],v[3],v[4],v[5],v[6],v[7])
+#print(mydic['kc1'][0]['sid'])
+
+#print(mydic.keys())
+for i in mydic.values():
+    print(i)
+
+for k,v in mydic.items():
+    print(k)
