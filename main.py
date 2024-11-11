a12c0940ce4a23fd3ad1b223ae5100ad2f258fcc
sailing <176530355@qq.com>
2024-11-11 22:06:31 +0800

    yu

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ data=pd.read_json('./mycodegit/Soildata.json')
 df=pd.DataFrame(data)
 
 layer_dic={}
-
+layer_list=[]
 print(df.columns)
 for index in df.columns:
     for i in df[index]:
-        layer_dic[index]=SoilLayer(i['sid'],i['layer_name'],i['start'],i['end'],i['adjust'],i['qsi'],i['qua'])
+        layer_list.append(SoilLayer(i['sid'],i['layer_name'],i['start'],i['end'],i['adjust'],i['qsi'],i['qua']))
+    layer_dic[index]=layer_list
 
 print(layer_dic)
+
 print(type(layer_dic))
 
-soil=Soil('kc1',layer_dic['kc1'])
+sdf=pd.DataFrame(layer_dic)
+print(sdf)
 
-print(soil.dict)
+
+#soil=Soil('kc1',layer_dic['kc1'])
+
+#soil.dict['kc1'].show_message()
