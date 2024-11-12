95544ed60ca1ff0cc42775e6189817ba598201a2
sailing <176530355@qq.com>
2024-11-12 20:03:24 +0800

    yu

--- main.py
+++ main.py
@@ -3,35 +3,31 @@ import math
 from Pile import *
 from SoilLayer import *
 from Soil import *
-      
+
+#输出显示完整性    
 pd.set_option('display.max_rows',None)
 pd.set_option('display.max_columns',None)
 pd.set_option('display.max_colwidth',None)
 pd.set_option('display.expand_frame_repr',False)
+
+#读入json
 data=pd.read_json('./mycodegit/Soildata.json') 
-
-
 df=pd.DataFrame(data)
-
+#将读入json给soill类
+#new一个空字典
 layer_dic={}
-
-print(df.columns)
 for index in df.columns:
-    layer_list=[]
+    layer_list=[] #new一个list，放在这里重要性 第二次就重新建立new
     for i in df[index]:
         layer_list.append(SoilLayer(i['sid'],i['layer_name'],i['start'],i['end'],i['adjust'],i['qsi'],i['qua']))
     layer_dic[index]=layer_list
-
+#
 print(layer_dic)
-
-print('----')
 
 soildf=pd.DataFrame(layer_dic)
 
-print(soildf)
 
 soil1=Soil('kc1',layer_dic['kc1'])
-
 
 for i in soil1.dict:
     print(i.show_message(0.8))
