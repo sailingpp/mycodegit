371f1b8b77049cdb49b8c8b08586842db3d44736
sailing <176530355@qq.com>
2024-11-12 19:55:50 +0800

    yu

--- SoilLayer.py
+++ SoilLayer.py
@@ -14,7 +14,8 @@ class SoilLayer:
         self.length=start-end
         self.layer_qsi=qsi*self.length*adjust
 
-     def show_message(self):
-        print(f'{self.id}土,{self.layername},深度{self.length:0.3f},调整系数{self.adjust},测摩阻系数{self.qsi},端阻系数{self.qua}')
+     def show_message(self,diameter):
+        pile=Pile(diameter)
+        print(f'{self.id}土,{self.layername},深度{self.length:0.3f},调整系数{self.adjust},侧摩阻系数{self.qsi},侧摩阻力{pile.cir*self.layer_qsi:0.3f},端阻系数{self.qua},端阻力{pile.area*self.qua:0.3f}')
 
 
--- main.py
+++ main.py
@@ -4,26 +4,39 @@ from Pile import *
 from SoilLayer import *
 from Soil import *
       
+pd.set_option('display.max_rows',None)
+pd.set_option('display.max_columns',None)
+pd.set_option('display.max_colwidth',None)
+pd.set_option('display.expand_frame_repr',False)
 data=pd.read_json('./mycodegit/Soildata.json') 
+
+
 df=pd.DataFrame(data)
 
 layer_dic={}
-layer_list=[]
+
 print(df.columns)
 for index in df.columns:
+    layer_list=[]
     for i in df[index]:
         layer_list.append(SoilLayer(i['sid'],i['layer_name'],i['start'],i['end'],i['adjust'],i['qsi'],i['qua']))
     layer_dic[index]=layer_list
 
 print(layer_dic)
 
-print(type(layer_dic))
+print('----')
 
 soildf=pd.DataFrame(layer_dic)
 
 print(soildf)
 
-soil=Soil('kc1',layer_dic['kc1'])
+soil1=Soil('kc1',layer_dic['kc1'])
 
-for i in soil.dict:
-    print(i.show_message())
+
+for i in soil1.dict:
+    print(i.show_message(0.8))
+
+soil2=Soil('kc2',layer_dic['kc2'])
+
+for i in soil2.dict:
+    print(i.show_message(0.8))
